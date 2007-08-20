ad5656f0b1fb0e32427081ace0dfe931ee808ec5
Ziling Zhao <zhaoz@u.washington.edu>
2007-08-20 22:09:46 +0000

    Added some docstrings describing what some of the view functions do

--- IssueTracker/views.py
+++ IssueTracker/views.py
@@ -9,7 +9,9 @@ from django import newforms as forms
 args = { 'loggedIn' : False, }
 
 def setDefaultArgs(request):
-    #args['loggedIn'] = request.user.is_authenticated()
+    """
+    This is called by any view that needs to have default args set.
+    """
     args['user'] = request.user
 
 def index(request):
@@ -20,6 +22,13 @@ def index(request):
     return render_to_response('IssueTracker/index.html', args)
 
 def create(request):
+    """
+    This is the view called when the user is creating a new issue, not for 
+    posting comments.
+    Currently takes a request, and sets the reporter to whoever is logged in and
+    then saves it.
+    """
+    # TODO improve the form validation here
     # FIXME need to add the ability to choose Group and Item 
     setDefaultArgs(request)
     if request.method == 'POST':
@@ -39,11 +48,17 @@ def create(request):
 create = permission_required('IssueTracker.add_issue')(create)
 
 def view(request, issue_id):
+    """
+    This is called when the issue requests a specific issue.
+    Basically displays the requested issue and any related comments. Offers the
+    user the ability to add comments to the issue.
+    """
     setDefaultArgs(request)
     # get the issue
     issue = get_object_or_404(Issue, pk=issue_id)
     args['issue'] = issue
 
+    # TODO need to make sure that this is being sorted by date
     args['comments'] = IssuePost.objects.filter(issue=issue)
 
     # get the forms
@@ -53,22 +68,32 @@ def view(request, issue_id):
 view = permission_required('IssueTracker.can_view')(view)
 
 def post(request, issue_id):
+    """
+    This is for the posting of comments to issues, it is also used to update
+    issues states and assignee
+    """
+    issue_id = int(issue_id)
+    print "post called with %d" % (issue_id)
     setDefaultArgs(request)
     if request.method == 'POST':
         issue = get_object_or_404(Issue, pk=issue_id)
         newComment = AddCommentForm(request.POST)
         updateIssue = UpdateIssueForm(request.POST)
 
-        fields=('issue_id','assignee','cc','resolve_time',
-                'resolved_state', 'last_modified')
-        issue = updateIssue.save(commit=False)
-        issue.issue_id=issue_id
+        #fields=('issue_id','assignee','cc','resolve_time',
+                #'resolved_state', 'last_modified')
+        updateIssue = updateIssue.save(commit=False)
+        if (updateIssue.assignee is not None):
+            issue.assignee = updateIssue.assignee
+        if (updateIssue.resolved_state is not None):
+            issue.resolved_state = updateIssue.resolved_state
+        issue.save()
 
-
-        newComment.save(commit=False)
+        print type(newComment)
+        newComment = newComment.save(commit=False)
         newComment.issue = issue
         newComment.user = request.user
-
+        #newComment.save()
 
         #if obj.is_valid():
             #issue = obj.save()
@@ -84,6 +109,10 @@ def post(request, issue_id):
 post = permission_required('IssueTracker.add_issuepost')(post)
 
 def allIssues(request):
+    """
+    Currently, all this does is grab a list of issues from the db and dumps it
+    to template
+    """
     setDefaultArgs(request)
     issueList = Issue.objects.all().order_by('-last_modified')
     args['issueList'] = issueList
