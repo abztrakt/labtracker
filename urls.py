fbcbf6a5945ed000dd34a8418b6eb8328d7e6601
Ziling Zhao <zhaoz@u.washington.edu>
2007-08-22 22:01:29 +0000

    moved css to repo, changed so /static is no longer needed to get to static, /css and /js are available

--- urls.py
+++ urls.py
@@ -12,8 +12,12 @@ urlpatterns = patterns('',
     # (r'^labtracker/', include('labtracker.foo.urls')),
 
     # Uncomment this for admin:
+    (r'^css/(?P<path>.*)$', 'django.views.static.serve', 
+        {'document_root': '/var/www/django_apps/labtracker/static/css/', 'show_indexes': True}),
+    (r'^js/(?P<path>.*)$', 'django.views.static.serve', 
+        {'document_root': '/var/www/django_apps/labtracker/static/js/', 'show_indexes': True}),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', 
-        {'document_root': '/var/www/static/django/', 'show_indexes': True}),
+        {'document_root': '/var/www/django_apps/labtracker/static/', 'show_indexes': True}),
     (r'^admin/', include('django.contrib.admin.urls')),
     (r'^databrowse/(.*)', databrowse.site.root),
     (r'^$', include('labtracker.IssueTracker.urls')),
