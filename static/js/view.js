9bc1b59608707282bf86e1218d8d8fd03dd5e7cf
Ziling Zhao <zhaoz@u.washington.edu>
2007-09-05 21:30:59 +0000

    Added support for some ajax operations such as the CC list and the group grabbing for the issue creation area

--- static/js/view.js
+++ static/js/view.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
-        $("form#update_cc")[0].style.display = "none";
-        $("#cc_list")[0].style.display = "block";
-
         $("a.dropCC").bind("click", dropCC); 
+        $("form#addCC").bind("submit", addCC); 
     }
 );
+
 
 /**
  * Takes an event, stops the default action, and then drops the given cc user from the
@@ -18,8 +17,11 @@ function dropCC(event) {
 
     user_id = item.id.match(/^cc_(\d+)$/)[1];
 
+    errorl = $('div#cc_box > div.block_if_js > ul.errorl')[0];
+    errorl.reset();
+
     $.ajax({
-        'datatype':'json',
+        'dataType':'json',
         'url': "modIssue/",
         'type': 'POST',
         'data': { 
@@ -28,10 +30,59 @@ function dropCC(event) {
             },
         'error': function (req, err, e) {
                 // TODO will need to append some error messages somewhere
+                errorl.addErr("Was unable to remove user from CC list.");
             },
         'success': function (data) {
                 // delete the object
                 $(item).parent().remove()
             },
     });
+}
+
+function addCC(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    btn = event.target;
+    user = $("input#addCC_user")[0].value;
+    
+    findRes = $("ul#cc_list > li > span").filter( 
+        function () { return this.innerHTML == user; } );
+
+
+    errorl = $('div#cc_box > div.block_if_js > ul.errorl')[0];
+    errorl.reset();
+    if (findRes.length > 0) {
+        console.log("Found dupes, will not submit");
+        errorl.addErr("User already in CC list");
+        return false;
+    }
+    console.log(findRes.length);
+    console.log("in addCC");
+
+    $.ajax({
+        'dataType':'json',
+        'url':'modIssue/',
+        'type': 'POST',
+        'data': {
+                'action': 'addcc',
+                'user': user,
+            },
+        'error': function (req, err, e) {
+                errorl.addErr("Could not add user to CC list, does user exist?");
+            },
+        'success': function (data) {
+                var btn = $(rmCCLink(data['userid'], data.username)).bind("click", dropCC);
+                var li = $("<li></li>").append(btn).append(" ").append("<span>" + 
+                    data['username'] + "</span>");
+                $("ul#cc_list").append(li);
+            },
+    });
+}
+
+/**
+ * Creates the the html for rm link
+ */
+function rmCCLink(userid, username) {
+    return '<a id="cc_' + userid + '" class="dropCC" title="Remove ' + username + 
+        ' from CC" href="#">x</a>';
 }
