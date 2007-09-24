aaf95144ead2729bc3eeb2fd32496974de194577
Ziling Zhao <zhaoz@u.washington.edu>
2007-09-24 23:21:55 +0000

    New widget types that kind of hack on the extra search portion

--- IssueTracker/forms.py
+++ IssueTracker/forms.py
@@ -15,26 +15,74 @@ searchFormFields = [
     ]
 searchFormFields.sort()
 
-# choices
+"""
+    modes = {}
+    modes['text'] = [
+        { 'name': 'contains', 'value': 'contains'},
+        { 'name': "doesn't contain", 'value': "doesn't contain"},
+        { 'name': 'is', 'value': 'is'},
+        { 'name': 'is not', 'value': 'is not'},
+        { 'name': 'begins with', 'value': 'begins with'},
+        { 'name': 'ends with', 'value': 'ends with'},
+    ]
+
+    modes['select'] = [
+        { 'name': 'is', 'value': 'is'},
+        { 'name': 'is not', 'value': 'is not'},
+    ]
+
+    modes['int'] = [
+        { 'name': 'is', 'value': 'is'},
+        { 'name': 'is not', 'value': 'is not'},
+        { 'name': 'Less than', 'value': '<'},
+        { 'name': 'Greater than', 'value': '>'},
+    ]
+"""
+
+# create the mode widgets
+
+class IsOrNot(forms.widgets.TextInput):
+    mode = forms.widgets.Select( choices = [ ('is', 'is'), ('is not', 'is not')])
+
+    def render(self, name, value, attrs=None):
+        return  self.mode.render("mode_%s" % name, "") + super(type(self), self).render(name, value, attrs)
+
+class FuzzySearch(forms.widgets.TextInput):
+    mode = forms.widgets.Select( choices = [ 
+        ('contains', 'contains'), ('not contain', "doesn't contain"),
+        ('is', 'is'), ('is not', 'is not'), ('begins with', 'begins with'),
+        ('ends with', 'ends with')])
+
+    def render(self, name, value, attrs=None):
+        return  self.mode.render("mode_%s" % name, "") + super(type(self), self).render(name, value, attrs)
+
+class IsSelected(forms.widgets.CheckboxSelectMultiple):
+    mode = forms.widgets.Select( choices = [ ('is', 'is'), ('is not', 'is not')])
+
+    def render(self, name, value, attrs=None):
+        return  self.mode.render("mode_%s" % name, "") + super(type(self), self).render(name, value, attrs)
+
+
 
 class SearchForm(forms.Form):
     """
     """
-    issue_id = forms.CharField()
-    title = forms.CharField()
-    description = forms.CharField()
+    #issue_id = STestField()
+    issue_id = forms.CharField(widget=IsOrNot)
+    title = forms.CharField(widget=FuzzySearch)
+    description = forms.CharField(widget=FuzzySearch)
     resolve_time = forms.CharField()
     post_time = forms.CharField()
-    reporter = forms.CharField()
-    cc = forms.CharField()
+    reporter = forms.CharField(widget=FuzzySearch)
+    cc = forms.CharField(widget=FuzzySearch)
+    assignee = forms.CharField(widget=FuzzySearch)
+    item = forms.CharField(widget=FuzzySearch)
 
-    resolved_state = forms.MultipleChoiceField(widget = forms.widgets.CheckboxSelectMultiple) 
-    problem_type = forms.MultipleChoiceField(widget = forms.widgets.CheckboxSelectMultiple) 
-    group = forms.MultipleChoiceField(widget = forms.widgets.CheckboxSelectMultiple) # TODO choices = 
-    inventory_type = forms.MultipleChoiceField(widget = forms.widgets.CheckboxSelectMultiple)
+    resolved_state = forms.MultipleChoiceField(widget = IsSelected) 
+    problem_type = forms.MultipleChoiceField(widget = IsSelected) 
+    group = forms.MultipleChoiceField(widget = IsSelected) # TODO choices = 
+    inventory_type = forms.MultipleChoiceField(widget = IsSelected)
 
-    assignee = forms.MultipleChoiceField() # TODO choices = 
-    item = forms.MultipleChoiceField() # TODO choices = 
 
 
 # like IssueForm, but with the primary key as well
