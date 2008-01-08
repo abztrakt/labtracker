01a5c6c7bb49614cec7c0c344edef2ff28b04572
Ziling Zhao <zhaoz@u.washington.edu>
2008-01-08 19:20:08 +0000

    cleaned up, removed databrowse

--- urls.py
+++ urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls.defaults import *
 from django.contrib import databrowse
-from labtracker.Machine.models import *
-#from labtracker.LabtrackerCore.models import *
+import labtracker.Machine.models as machine
 
-#databrowse.site.register(labtracker.Machine.model.Machine)
-#databrowse.site.register(Item)
-databrowse.site.register(Machine)
 
 urlpatterns = patterns('',
     # Example:
@@ -18,6 +14,7 @@ urlpatterns = patterns('',
         {'document_root': '/var/www/django_apps/labtracker/static/js/', 'show_indexes': True}),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', 
         {'document_root': '/var/www/django_apps/labtracker/static/', 'show_indexes': True}),
+    # (r'^admin/Machine/group/add/$', 'labtracker.Machine.admin_views.addGroup'),
     (r'^admin/', include('django.contrib.admin.urls')),
     (r'^databrowse/(.*)', databrowse.site.root),
     #(r'^$', include('labtracker.IssueTracker.urls')),
