3592915d014d89a32718e31de6e3a73730aee186
Ziling Zhao <zhaoz@u.washington.edu>
2008-02-21 22:58:33 +0000

    urls now hooked up to new scripts for group creation/modifiction.
    Group creation and modification is now working, works pretty much like the add/remove item stuff.
    
    Few things that would be nice to implement, like editing the contacts for the groups

--- Machine/admin_views.py
+++ Machine/admin_views.py
@@ -11,11 +11,140 @@ import Machine.models as machineModels
 import LabtrackerCore.models as coreModels
 import LabtrackerCore.forms as coreForms
 
-register = template.Library()
+###################################
+#  Methods to modify/add Items    #
 
-def item(request, core_form, model_form, change=False, add=False):
+@staff_member_required
+def modItem(request, id):
+    """
+    Modify a pre-existing Machine.Item
+    Takes request and the id 
+    """
+    machine_item = get_object_or_404(machineModels.Item, pk=id)
+    core_item = machineItem.item
 
-    model = machineModels.Item
+    if request.method == "POST":
+        # If the user has submitted stuff, form with stuff filled in
+        model_form = MachineForm(request.POST, instance=machineItem)
+        core_form = coreForms.BaseItemForm(request.POST, instance=core_item)
+
+        if core_form.is_valid() and model_form.is_valid():
+            model_form.save()
+            core_form.save()
+
+            # TODO implement redirection depending on which button pressed
+            return http.HttpResponseRedirect('/admin/Machine/item/')
+        else: 
+            # TODO do tricky error handling stuff like the global error message
+            # that admin page has by default
+            pass
+
+    else:
+        core_form = coreForms.BaseItemForm(instance=core_item)
+        model_form = MachineForm(instance=machine_item)
+
+    return adminRender(request, machineModels.Item, core_form, model_form, change=True)
+
+
+@staff_member_required
+def addItem(request):
+    """
+    Add a new Machine.Item, takes nothing but the request object
+    """
+    if request.method == "POST":
+        data = request.POST.copy()
+
+        model_form = MachineForm(data)
+        core_form = coreForms.BaseItemForm(data)
+
+        if core_form.is_valid() and model_form.is_valid():
+            new_machine = model_form.save(commit=False)
+            new_machine.save(data['name'])
+
+            # TODO implement redirection depending on which button pressed
+            return http.HttpResponseRedirect('/admin/Machine/item/')
+        else:
+            # TODO do tricky error handling stuff like the global error message
+            # that admin page has by default
+            pass
+
+    else:
+        model_form = MachineForm()
+        core_form = coreForms.BaseItemForm()
+
+    return adminRender(request, machineModels.Item, core_form, model_form, add=True)
+
+
+###################################
+
+###################################
+#  Methods to modify/add Groups   #
+
+@staff_member_required
+def modGroup(request, id):
+    model = machineModels.Group
+    machine_group = get_object_or_404(model, pk=id)
+    core_item = machine_group.group
+
+    if request.method == "POST":
+        model_form = MachineGroupForm(request.POST, instance=machine_group)
+        core_form = coreForms.BaseGroupForm(request.POST, instance=core_item)
+
+        if core_form.is_valid() and model_form.is_valid():
+            model_form.save()
+            core_form.save()
+
+            # TODO implement redirection depending on which button pressed
+            return http.HttpResponseRedirect('/admin/Machine/group/')
+        else:
+            # TODO do tricky error handling stuff like the global error message
+            # that admin page has by default
+            pass
+    else:
+        core_form = coreForms.BaseGroupForm(instance=core_item)
+        model_form = MachineGroupForm(instance=machine_group)
+
+    return adminRender(request, model, core_form, model_form, change=True)
+
+
+@staff_member_required
+def addGroup(request):
+    model = machineModels.Group
+
+    if request.method == "POST":
+        data = request.POST.copy()
+
+        model_form = MachineGroupForm(data)
+        core_form = coreForms.BaseGroupForm(data)
+
+        if core_form.is_valid() and model_form.is_valid():
+            new_machine_group = model_form.save(commit=False)
+            new_machine_group.save(data['name'], data['description'])
+
+            # TODO implement redirection depending on which button pressed
+            return http.HttpResponseRedirect('/admin/Machine/group/')
+        else:
+            # TODO do tricky error handling stuff like the global error message
+            # that admin page has by default
+            pass
+    else:
+        model_form = MachineGroupForm()
+        core_form = coreForms.BaseGroupForm()
+
+    return adminRender(request, model, core_form, model_form, add=True)
+
+###################################
+
+
+###################################
+#         helper methods
+
+def adminRender(request, model, core_form, model_form, change=False, add=False):
+    """
+    Utilized as a wrapper to the administration templates, creates the variables
+    needed by the form.
+    """
+
     opts = model._meta
 
     context = template.RequestContext(request, {
@@ -34,65 +163,13 @@ def item(request, core_form, model_form, change=False, add=False):
     context.update(submit_row(context))
 
     return render_to_response(
-        "admin/Machine/item/change_form.html",
+        "Machine/admin/change_form.html",
         {
             'addForm'           : model_form,
             'coreForm'          : core_form,
         },
         context
     )
-
-@staff_member_required
-def modItem(request, id):
-    machineItem = get_object_or_404(machineModels.Item, pk=id)
-    coreItem = machineItem.item
-
-    if request.method == "POST":
-        model_form = MachineForm(request.POST, instance=machineItem)
-        core_form = coreForms.BaseItemForm(request.POST, instance=coreItem)
-
-        if core_form.is_valid() and model_form.is_valid():
-            model_form.save()
-            core_form.save()
-
-            # TODO redirect like the admin page does 
-            return http.HttpResponseRedirect('/admin/Machine/item/')
-        else:
-            print "Error happened while trying to validate"
-    else:
-        core_form = coreForms.BaseItemForm(instance=coreItem)
-        model_form = MachineForm(instance=machineItem)
-
-    return item(request, core_form, model_form, change=True)
-
-
-
-
-@staff_member_required
-def addItem(request):
-    if request.method == "POST":
-        data = request.POST.copy()
-
-        model_form = MachineForm(data)
-        core_form = coreForms.BaseItemForm(data)
-
-        if core_form.is_valid() and model_form.is_valid():
-            new_machine = model_form.save(commit=False)
-            new_machine.save(data['name'])
-
-            # TODO redirect like the admin page does 
-            return http.HttpResponseRedirect('/admin/Machine/item/')
-        else:
-            print "Error happened while trying to validate"
-
-    else:
-        model_form = MachineForm()
-        core_form = coreForms.BaseItemForm()
-
-    return item(request, core_form, model_form, add=True)
-
-
-# helper methods
 
 def submit_row(context):
     opts = context['opts']
@@ -112,5 +189,11 @@ def submit_row(context):
 class MachineForm(ModelForm):
     class Meta:
         model = machineModels.Item
-        fields=('id', 'item_id', 'mt', 'ms', 'ml', 'ip', 'mac', 'date_added', 'manu_tag', 'comment')
+        fields = ('id', 'item_id', 'mt', 'ms', 'ml', 'ip', 'mac', 'date_added',
+                'manu_tag', 'comment')
+
+class MachineGroupForm(ModelForm):
+    class Meta:
+        model = machineModels.Group
+        fields = ('is_lab', 'casting_server', 'gateway')
 
