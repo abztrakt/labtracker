0bddd26867a798ed2c65d3309c9cd394216a150b
Ziling Zhao <zhaoz@u.washington.edu>
2008-05-01 21:33:24 +0000

    Media root should use APP_DIR

--- settings.sample.py
+++ settings.sample.py
@@ -37,7 +37,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/var/www/static/django/'
+MEDIA_ROOT = '%s/static' % APP_DIR
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
