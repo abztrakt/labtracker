865446c8911bf920f7f6916ce01b955ea796d19c
Ziling Zhao <zhaoz@u.washington.edu>
2008-12-15 23:29:49 +0000

    use POST for all CC Updates

--- IssueTracker/static/js/view.js
+++ IssueTracker/static/js/view.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-        $("a.dropCC").bind("click", dropCC); 
-        $("form#addCC").bind("submit", addCC); 
-        $('#problem_type_clicker')[0].compactor.setState(0);
-    }
+		$("a.dropCC").bind("click", dropCC); 
+		$("form#addCC").bind("submit", addCC); 
+		$('#problem_type_clicker')[0].compactor.setState(0);
+	}
 );
 
 
@@ -10,31 +10,32 @@ $(document).ready(function() {
  * Takes an event, stops the default action, and then drops the given cc user from the
  * list, and then removes that from the list
  *
- * @param event     The event that triggered dropCC
+ * @param event	 The event that triggered dropCC
  */
 function dropCC(event) {
-    event.preventDefault();
-    item = event.target;
+	event.preventDefault();
+	item = event.target;
 
-    user_id = item.id.match(/^cc_(\d+)$/)[1];
+	user_id = item.id.match(/^cc_(\d+)$/)[1];
 
-    errorl = $('div#cc_box > div.block_if_js > ul.errorl')[0];
+	errorl = $('div#cc_box > div.block_if_js > ul.errorl')[0];
 
-    $.ajax({
-        'url': "modIssue/",
-        'data': { 
-                'action'    : 'dropcc',
-                'user'      : user_id
-            },
-        'error': function (req, err, e) {
-                errorl.addErr("Was unable to remove user from CC list.");
-            },
-        'success': function (data) {
-                // delete the object
-                $(item).parent().remove()
-                reloadHistory();
-            },
-    });
+	$.ajax({
+		'url': "modIssue/",
+		'type': "POST",
+		'data': { 
+				'action'	: 'dropcc',
+				'user'	  : user_id
+			},
+		'error': function (req, err, e) {
+				errorl.addErr("Was unable to remove user from CC list.");
+			},
+		'success': function (data) {
+				// delete the object
+				$(item).parent().remove()
+				reloadHistory();
+			},
+	});
 }
 
 /**
@@ -42,41 +43,41 @@ function dropCC(event) {
  * succesful, will append item to CC list on user side, otherwise appends some errors to
  * local errorl.
  *
- * @param event     The event that triggered addCC
+ * @param event	 The event that triggered addCC
  */
 function addCC(event) {
-    event.preventDefault();
-    btn = event.target;
-    user = $("input#addCC_user")[0].value;
-    
-    findRes = $("ul#cc_list > li > span").filter( 
-        function () { return this.innerHTML == user; } );
+	event.preventDefault();
+	btn = event.target;
+	user = $("input#addCC_user")[0].value;
+	
+	findRes = $("ul#cc_list > li > span").filter( 
+		function () { return this.innerHTML == user; } );
 
 
-    errorl = $('div#cc_box > div.block_if_js > ul.errorl')[0];
+	errorl = $('div#cc_box > div.block_if_js > ul.errorl')[0];
 
-    if (findRes.length > 0) {
-        errorl.addErr("User already in CC list");
-        return false;
-    }
+	if (findRes.length > 0) {
+		errorl.addErr("User already in CC list");
+		return false;
+	}
 
-    $.ajax({
-        'url':'modIssue/',
-        'data': {
-                'action'    : 'addcc',
-                'user'      : user,
-                'js'        : 1
-            },
-        'error': function (req, err, e) {
-                errorl.addErr("'" + user + "' could not be added to CC list. Please make sure '" + user + "' exists.");
-            },
-        'success': function (data) {
-                reloadCcList();
+	$.ajax({
+		'url':'modIssue/',
+		'type': 'POST',
+		'data': {
+				'action'	: 'addcc',
+				'user'	  : user
+			},
+		'error': function (req, err, e) {
+				errorl.addErr("'" + user + "' could not be added to CC list. Please make sure '" + user + "' exists.");
+			},
+		'success': function (data) {
+				reloadCcList();
 
-                // reload the history
-                reloadHistory();
-            },
-    });
+				// reload the history
+				reloadHistory();
+			},
+	});
 }
 
 // TODO updateHistory, only retrieve stuff we don't have
@@ -88,35 +89,35 @@ function updateHistory(last) {
  * server
  */
 function reloadHistory() {
-    $('#history_box').empty();
-    $.ajax({
-        'url'       : 'fetch/',
-        'dataType'  : 'text',
-        'data'      : {
-                'format'    : 'html',
-                'req'       : 'history'
-            },
-        'success'   : function (text) {
-                $('#history_box').append(text);
-            }
-    });
+	$('#history_box').empty();
+	$.ajax({
+		'url'	   : 'fetch/',
+		'dataType'  : 'text',
+		'data'	  : {
+				'format'	: 'html',
+				'req'	   : 'history'
+			},
+		'success'   : function (text) {
+				$('#history_box').append(text);
+			}
+	});
 }
 
 /**
  * Reload the CC list, removes everything and request a new box from server
  */
 function reloadCcList() {
-    $('#cc_box').empty();
-    $.ajax({
-        'url'       : 'fetch/',
-        'dataType'  : 'text',
-        'data'      : {
-                'req'       : 'cclist'
-            },
-        'success'   : function (text) {
-                var cc_box = $('#cc_box').html(text);
-                initializeJavascript(cc_box);
-                $('.dropCC').bind('click', dropCC);
-            }
-    });
+	$('#cc_box').empty();
+	$.ajax({
+		'url'	   : 'fetch/',
+		'dataType'  : 'text',
+		'data'	  : {
+				'req'	   : 'cclist'
+			},
+		'success'   : function (text) {
+				var cc_box = $('#cc_box').html(text);
+				initializeJavascript(cc_box);
+				$('.dropCC').bind('click', dropCC);
+			}
+	});
 }
