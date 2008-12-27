c314ab081b35c0f3e678cab73a2fe09c98cb7173
Ziling Zhao <zhaoz@u.washington.edu>
2008-12-27 02:07:35 +0000

    some css cleanup

--- static/js/global.js
+++ static/js/global.js
@@ -32,8 +32,8 @@ $(document).ready(function() {
             var item = $(this);
             $.extend(this, compactor);
             this.compactor.parent = this;
-            $.extend(item.children('.compact_trigger')[0], { 'parent': this });
-            item.children('.compact_trigger').click(function (e) {
+            $.extend(item.children('.compact-trigger')[0], { 'parent': this });
+            item.children('.compact-trigger').click(function (e) {
                     this.parent.compactor.toggle();
                 });
         });
@@ -50,11 +50,11 @@ var compactor = {
 
             if (g_state == 0) {     // compact everything
                 // TODO
-                $(this.parent).children('.compact').addClass('display_none');
+                $(this.parent).children('.compact').addClass('display-none');
                 this.state = 0;
             } else {                // expand
                 // TODO
-                $(this.parent).children('.compact').removeClass('display_none');
+                $(this.parent).children('.compact').removeClass('display-none');
                 this.state = 1;
             }
         },
