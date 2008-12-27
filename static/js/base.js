d53d1222166098e772cb4b0d711d2d7a643a03de
Ziling Zhao <zhaoz@u.washington.edu>
2008-12-27 03:59:44 +0000

    refactor, some validation fixes, etc.

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,5 @@
 $(function () {
-	$('#fast_search #search_term').focus(function (eve) {
+	$('#fast-search #search_term').focus(function (eve) {
 		// highlight the inner text
 		eve.target.select();
 	});
