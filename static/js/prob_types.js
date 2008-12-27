8e19fd1b7655c6d8488a365ec9e2af5f257dcd04
Ziling Zhao <zhaoz@u.washington.edu>
2008-12-27 03:59:46 +0000

    deprecation, and indentation fix

--- static/js/prob_types.js
+++ static/js/prob_types.js
@@ -1,41 +1,42 @@
+// @deprecated 
 $(document).ready(function () {
 	$('li.tag').click( tag_changeTagState );
 
     // Extend the li.tag objects with some power
     $('li.tag').each( function () {
         $.extend(this, {
-                'state'         : 0,    // 0 is untagged, 1 is tagged
-                'setState'      : function (g_state) {
-                    if (g_state == this.state) { return; }
+			'state'         : 0,    // 0 is untagged, 1 is tagged
+			'setState'      : function (g_state) {
+				if (g_state == this.state) { return; }	// already set
 
-                    var item = $(this);
-                    item.unbind('click');
-                    item.remove();
+				var item = $(this);
+				item.unbind('click');
+				item.remove();
 
-                    var box;
-		            var select =
-                    $('#id_problem_type').children(':contains('+item.text()+')');
+				var box;
+				var select = $('#id_problem_type')
+					.children(':contains('+item.text()+')');
 
-                    if (g_state) {      // switch on
-                        this.state = 1;
-                        box = $('#tagged ul');
-                        select.attr('selected', 'selected');
-                    } else {            // switch off
-                        box = $('#untagged ul');
-                        this.state = 0;
-                        select.attr('selected', '');
-                    }
-                    box.append(item);
-                    item.toggleClass('tagged');
-                    item.click( tag_changeTagState );
-                },
-                'toggleState'   : function () {
-                    this.setState(!this.state);
-                },
-                'getState'      : function () {
-                    return this.state;
-                }
-            });
+				if (g_state) {      // switch on
+					this.state = 1;
+					box = $('#tagged ul');
+					select.attr('selected', 'selected');
+				} else {            // switch off
+					box = $('#untagged ul');
+					this.state = 0;
+					select.attr('selected', '');
+				}
+				box.append(item);
+				item.toggleClass('tagged');
+				item.click( tag_changeTagState );
+			},
+			'toggleState'   : function () {
+				this.setState(!this.state);
+			},
+			'getState'      : function () {
+				return this.state;
+			}
+		});
     });
 
     // sync
