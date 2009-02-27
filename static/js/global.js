552a8b17daeb2e9c73fb1f5aa4a3e88bd05ec541
Ziling Zhao <zhaoz@u.washington.edu>
2009-02-27 23:12:18 +0000

    removed some old js cruft

--- static/js/global.js
+++ static/js/global.js
@@ -27,42 +27,8 @@ $(document).ready(function() {
                 };
             } 
         );
-
-        $.each($(".compact_container"), function () {
-            var item = $(this);
-            $.extend(this, compactor);
-            this.compactor.parent = this;
-            $.extend(item.children('.compact-trigger')[0], { 'parent': this });
-            item.children('.compact-trigger').click(function (e) {
-                    this.parent.compactor.toggle();
-                });
-        });
-
     }
 );
-
-var compactor = {
-    'compactor' : {
-        'state'     : 1,
-        'setState'  : function (g_state) {
-            if (this.state == g_state)
-                return;
-
-            if (g_state == 0) {     // compact everything
-                // TODO
-                $(this.parent).children('.compact').addClass('display-none');
-                this.state = 0;
-            } else {                // expand
-                // TODO
-                $(this.parent).children('.compact').removeClass('display-none');
-                this.state = 1;
-            }
-        },
-        'toggle'     : function () {
-            this.setState(!this.state);
-        }
-    }
-};
 
 var django_date_parser = {
 	'id':		'django_date',
