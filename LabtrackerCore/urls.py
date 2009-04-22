a9baebb425470dc7fd7cb40cf48057e982c1bc0a
Craig M. Stimmel <cstimmel@uw.edu>
2009-04-22 17:54:39 +0000

    Removing excess '.' from the urlpatterns so things will load.

--- LabtrackerCore/urls.py
+++ LabtrackerCore/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     url(r'^$', 'LabtrackerCore.views.userPrefs', name='userPrefs'),
 )
 
-urlpatterns += patterns('django.contrib.auth.views.',
+urlpatterns += patterns('django.contrib.auth.views',
     url(r'^pwchange/$', 'password_change', 
         {'template_name': 'pwchange.html'}, name="pwchange"),
     (r'^pwchange/done/$', 'password_change_done', {'template_name': 'pwchange_done.html'}),
