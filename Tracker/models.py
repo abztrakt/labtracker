009bc1d50be636b82736c45e0d717273dbb61b2e
Eric Wu <ericwu@u.washington.edu>
2009-08-03 23:11:14 +0000

    Created models for login/out statistics script.

--- Tracker/models.py
+++ Tracker/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Statistics(models.Model):
+    login_time = models.DateTimeField()
+    logout_time = models.DateTimeField(auto_now=True, null=True, verbose_name='Logout Time')
+    mac1 = models.CharField(max_length=17, verbose_name='MAC Address')
+    netid = models.CharField(max_length=20, verbose_name='UW Net ID')
+    #regid = models.CharField(max_length=32, verbose_name='UW Reg ID')
