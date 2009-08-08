90d6a19c46899647361e0bfcfb251522ace00584
Eric Wu <ericwu@u.washington.edu>
2009-08-08 00:41:54 +0000

    Added linux portion of client script

--- client_scripts/labtracker.py
+++ client_scripts/labtracker.py
@@ -1,7 +1,25 @@
 import urllib
 import urllib2
 import getpass
+import sys
+import os
 
+def get_mac():
+    # windows systems
+    if sys.platform == 'win32':
+        for line in os.popen("ipconfig /all"):
+            if line.lstrip().startswith('Physical Address'):
+                mac = line.split(':')[1].strip().replace('-',':')
+                break
+        return mac
+    # linux/apple systems
+    else:
+        for line in os.popen("/sbin/ifconfig"):
+            if line.find('Ether') > -1:
+                mac = line.split()[4]
+            break
+        return mac 
+    
 LABTRACKER_URL = "labtracker.eplt.washington.edu"
 
 ACTIONS = ('login','logout','ping')
@@ -13,15 +31,10 @@ ACTIONS = ('login','logout','ping')
 user = getpass.getuser()
 data = urllib.urlencode({'user': user})
 
+# TODO ensure that the secure url is accessible
+# TODO ensure that the post data (in variable 'data') can be sent
 try:
-    urllib2.openurl("https://%s/%s/%s" % (LABTRACKER_URL, ACTIONS[0], get_mac()), data)
+    urllib2.urlopen("http://%s/%s/%s" % (LABTRACKER_URL, ACTIONS[0], get_mac()))
 except:
     pass
 
-def get_mac():
-    # works on windows only
-    for line in os.popen("ipconfig /all"):
-        if line.lstrip().startswith('Physical Address'):
-            mac = line.split(':')[1].strip().replace('-',':')
-            break
-    return mac
