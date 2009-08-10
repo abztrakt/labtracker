2c65d455f04797b079a46837b718f2a55e7e0587
Taylor McKay <tmckay05@u.washington.edu>
2009-08-10 20:33:12 +0000

    added action option to client script and CLI support

--- client_scripts/labtracker.py
+++ client_scripts/labtracker.py
@@ -3,9 +3,22 @@ import urllib2
 import getpass
 import sys
 import os
+from optparse import OptionParser
 
 LABTRACKER_URL = "labtracker.eplt.washington.edu"
 ACTIONS = ('login','logout','ping')
+
+# functions at the module level must be defined
+# before being called ;) 
+def get_actions_list():
+    global ACTIONS
+    return ', '.join(ACTIONS)
+
+parser = OptionParser()
+parser.add_option("-a", "--action", dest="action", 
+                help="Action of script: %s" % get_actions_list()) 
+
+(options, args) = parser.parse_args()
 
 def get_mac():
     # windows systems
@@ -23,18 +36,32 @@ def get_mac():
             break
         return mac 
 
-# get user info from machine
-user = getpass.getuser()
-data = urllib.urlencode({'user': user})
+def get_data(): 
+    # get user info from machine
+    user = getpass.getuser()
+    data = urllib.urlencode({'user': user})
 
-# TODO ensure that the secure url is accessible
-# TODO ensure that the post data (in variable 'data') can be sent
+def track():
+    global ACTIONS, options
+    if hasattr(options, "action"):
 
-# with data argument, request is automatically POST
-try:
-    urllib2.Request(url="http://%s/tracker/%s/%s" % (LABTRACKER_URL, 
-                                                    ACTIONS[0], get_mac()),
-                    data=data)
-except:
-    pass
+        # verify that option.action is a valid action
+        if options.action in ACTIONS:  
+            # TODO ensure that the secure url is accessible
+            # TODO ensure that the post data (in variable 'data') can be sent
+            
+            # with data argument, request is automatically POST
+            try:
+                urllib2.Request(url="http://%s/tracker/%s/%s" % (LABTRACKER_URL, 
+                                                                options.action, get_mac()),
+                                data=get_data())
+            except:
+                pass
+        else:
+            print 'Action attribute not valid. Actions are %s.' % get_actions_list()
+    else:
+        print 'Need action argument: %s' % get_actions_list()
+       
 
+if __name__ == '__main__':
+    track() 
