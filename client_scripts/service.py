e287eb3b28966d3a46af96af5f0bf6339a1450b9
Taylor McKay <tmckay05@u.washington.edu>
2009-08-19 22:31:54 +0000

    added better error handling and attempt to catch login/logout to service

--- client_scripts/service.py
+++ client_scripts/service.py
@@ -1,12 +1,15 @@
 """
 labtracker service
 
+To install:
+    run 'cmd' as admin    
+
 Usage:
     c:\Python26\python.exe service.py install
     c:\Python26\python.exe service.py start 
+    c:\Python26\python.exe service.py remove
 
-To remove:
-    run 'cmd' as admin    
+Also:
     sc stop labtracker
     sc delete labtracker
 """
@@ -31,9 +34,26 @@ class Labtracker(ServiceFramework):
         self.stop_event = win32event.CreateEvent(None, 0, 0, None)
         self.isAlive = True
 
+    def ServiceCtrlHandler(self, status):
+        # login
+        if status == win32service.WIN32_SERVICE_CONTROL_SESSIONCHANGE:
+            try:
+                urllib2.urlopen('http://inspiredlychee.eplt.washington.edu/labtrackerlogin.html')
+            except Exception, e:
+                pass
+
+        # shutdown
+        elif status == win32service.WIN32_SERVICE_CONTROL_PRESHUTDOWN:
+            try:
+                urllib2.urlopen('http://inspiredlychee.eplt.washington.edu/labtrackerlogout.html')
+            except Exception, e:
+                pass
+
     def SvcDoRun(self):
         import servicemanager
 
+        # listen for login/logout 
+        servicemanager.RegisterServiceCtrlHandler(self._svc_name_, self.ServiceCtrlHandler)
            
         self.ReportServiceStatus(win32service.SERVICE_RUNNING)
 
@@ -55,13 +75,17 @@ class Labtracker(ServiceFramework):
     def SvcStop(self):
         import servicemanager
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-        win32event.SetEvent(self.stop_event) 
         self.isAlive = False
+        try:
+            urllib2.urlopen('http://inspiredlychee.eplt.washington.edu/labtrackerlogoff.html')
+        except Exception, e:
+            pass
+        win32event.SetEvent(self.stop_event) 
         self.ReportServiceStatus(win32service.SERVICE_STOPPED)
         #servicemanager.LogInfoMsg('labtracker - received stop signal')
         #self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
         #win32event.SetEvent(self.hWaitStop)
 
 if __name__ == '__main__':
-    win32api.SetConsoleCtrlHandler(lambda x: True, True)
+    #win32api.SetConsoleCtrlHandler(lambda x: True, True)
     HandleCommandLine(Labtracker)
