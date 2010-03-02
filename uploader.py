2ce32c91c76c446a3017c52f042a88e84e470756
Eric Wu <ericwu@u.washington.edu>
2010-03-02 00:10:13 +0000

    Updated the uploader to suit the new, current compdata file

--- uploader.py
+++ uploader.py
@@ -12,8 +12,8 @@ import sys
 FORCE_DELETE = False
 
 def status(item, status):
-    print "name: %s, location: %s, ip: %s, casting server: %s, mac: %s, gateway: %s, service tag: %s" % (item['name'], item['location'], item['ip'], item['cast'], item['mac'], item['gateway'], item['service_tag'])
-    print " %s" % (status)
+    print "\nname: %s, location: %s, ip: %s, casting server: %s, mac: %s, gateway: %s, service tag: %s%s" % (item['name'], item['location'], item['ip'], item['cast'], item['mac'], item['gateway'], item['service_tag'], status)
+
 if not sys.argv[1]:
     print "Argument: file containing machine data to load into LabTracker."
     sys.exit(1)
@@ -50,9 +50,26 @@ location = ""
 segments = []
 item = {}
 for line in file.readlines():
-    if (line.startswith(";")):
-        item['location'] = re.sub("[^a-zA-Z0-9]", "", line[1:])
-        # save 
+
+    if re.match("^[a-zA-Z]", line): # row to input into database
+        segments = line.split("\t")
+        item['name'] = segments[0]
+        item['ip'] = segments[1]
+        item['cast'] = segments[2]
+        # MAC address is not in standard aa:bb:cc:dd:ee:ff format
+        if re.match("^[a-zA-Z0-9]{12}$", segments[3]):
+            item['mac'] = segments[3][0:2]
+            for i in range(1, len(segments[3]) / 2):
+                item['mac'] += ":" + segments[3][2*i:2*i+2]
+        else:
+            item['mac'] = "00:00:00:00:00:00"
+
+        item['gateway'] = segments[4]
+        item['service_tag'] = segments[6]
+        if not segments[6]:
+            item['service_tag'] = ""
+        
+        item['location'] = re.findall("^[a-zA-Z]*", segments[0])[0] # currently the only way to truly determine location. may be problematic with access+ locations.
         location = Location.objects.filter(name=item['location'])
         if location.count() < 1:
             location = Location(name=item['location'])
@@ -60,31 +77,6 @@ for line in file.readlines():
         else:
             location = location.get(name=item['location'])
 
-        m_group = Group.objects.filter(name=item['location'])
-        if m_group.count() < 1:
-            m_group = Group(name=item['location'], it=it, casting_server='100.0.0.0', gateway='100.0.0.0')
-            m_group.save()
-        else:
-            m_group = m_group.get(name=item['location'])
-
-    elif not re.match("^([ ]|\n|\t)", line):
-        # get segments of a line in a given location
-        segments = line.split("\t")
-        item['name'] = segments[0]
-        item['ip'] = segments[1]
-        item['cast'] = segments[2]
-        # MAC address is not in standard aa:bb:cc:dd:ee:ff format
-        item['mac'] = segments[3][0:2]
-        for i in range(1, len(segments[3]) / 2):
-            item['mac'] += ":" + segments[3][2*i:2*i+2]
-
-        item['gateway'] = segments[4]
-        item['service_tag'] = segments[5]
-        
-        m_group.casting_server = item['cast']
-        m_group.gateway = item['gateway']
-        
-        
         # save into models
         # save location into LabtrackerCore.Group, Machine.Group, Machine.Location
         machine = Item.objects.filter(name=item['name'])
@@ -101,7 +93,7 @@ for line in file.readlines():
                 status(item, "MAC exists, name doesn't exist, overwrite")
             else: # MAC doesn't exist or match, then add new machine
                 machine = Item(name=item['name'], type=type, it=it, location=location, ip=item['ip'], mac1=item['mac'], wall_port='unknown', manu_tag=item['service_tag'])
-                status(item, "MAC doesn't exist, name doesn't exist, add new")
+#                status(item, "MAC doesn't exist, name doesn't exist, add new")
         else: # else name does exist, check if MAC exists
             if item['mac'] == machine[0].mac1: # if MACs match, then overwrite
                 machine = machine.get(name=item['name'])
@@ -110,19 +102,20 @@ for line in file.readlines():
                 machine.ip = item['ip']
                 status(item, "MAC exists, name exists, overwrite")
             else: # else MACs are different, do not load and return error message
-                invalid = True
-                if FORCE_DELETE: # overwrite old machine with new data
-                    print "Force overwriting %s, new MAC: %s, overwritten MAC: %s" % (item['name'], item['mac'], machine[0].mac1)
-                    machine = machine[0]
+                machine = machine[0]
+                if machine.manu_tag == item['service_tag'] and not machine.mac2 and not machine.mac1 == item['service_tag']:
+                    print "Adding second MAC address %s to %s" % (item['mac'], item['name'])
+                    machine.mac2 = item['mac']
+                elif FORCE_DELETE: # overwrite old machine with new data
+                    print "Force overwriting %s, new MAC: %s, overwritten MAC: %s" % (item['name'], item['mac'], machine.mac1)
                     machine.mac1 = item['mac']
                     machine.manu_tag = item['service_tag']
                     machine.ip = item['ip']
                 else:
-                    print "Error writing %s with MAC address %s. Duplicate entry containing same machine name and different MAC address. Existing MAC address in database: %s" % (item['name'], item['mac'], machine[0].mac1)
+                    invalid = True
+                    print "Error writing %s with MAC address %s. Duplicate entry containing same machine name and different MAC address. Existing MAC address in database: %s" % (item['name'], item['mac'], machine.mac1)
 
         if not invalid or FORCE_DELETE:
             machine.save()
-            m_group.items.add(machine)
-            m_group.save()
             print "Successfully saved %s, %s" % (machine.name, machine.mac1)
 
