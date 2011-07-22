6d7f21a8ac96c3e866570cfb341a0e8f84437c6f
Michael <mnnguyen@uw.edu>
2011-07-22 11:30:50 -0700

    Fixed emailed bug regarding title = none and not having proper methods.

--- IssueTracker/Email.py
+++ IssueTracker/Email.py
@@ -6,7 +6,11 @@ import models
 class NewIssueEmail(Email):
 
     def __init__(self, issue, title=None, *args, **kwargs):
-	title = title.replace('@', '[at]')
+        try:
+            title = title.replace('@', '[at]')
+        except:
+            pass
+
         kwargs['subject'] = "[" + settings.EMAIL_SUBJECT_PREFIX + "]" + " #%d: %s " % (issue.pk, title)
 
         super(NewIssueEmail, self).__init__(sections=[], *args, **kwargs)
