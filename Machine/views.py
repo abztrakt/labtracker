de28d8a6d08166f4dc205360dc74a1b594d708f7
Craig M. Stimmel <cstimmel@uw.edu>
2011-08-25 16:47:19 -0700

    Aha - needed to pass a RequestContext to get MEDIA_URL to pass to the template so that CSS could actually be loaded.

--- Machine/views.py
+++ Machine/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 
 from Machine.models import Item, Location
 
 def list_by_location(request, location_name):
     location = Location.objects.get(name=location_name)
     object_list = Item.objects.filter(location=location)
-    return render_to_response('item_list.html', {'object_list':object_list,})
+    return render_to_response('item_list.html', {'object_list':object_list,},
+        context_instance=RequestContext(request))
