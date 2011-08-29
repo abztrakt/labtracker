b18b7f6daf0b0bf3f3776634750927c376b60e80
Vera Kobylchak <verakoby@uw.edu>
2011-08-29 16:24:05 -0700

    Fixes #309 -- Changed percentage significant digits to two decimal points.

--- LabtrackerCore/views.py
+++ LabtrackerCore/views.py
@@ -83,9 +83,9 @@ def dashboard(request):
         empty_locations = []
         for location in all_locations:
             try:
-                all_locations[location]['PercTotal'] = 100.0*(all_locations[location]['Usable'])/all_locations[location]['Total']
-                all_locations[location]['PercBroken'] = 100.0*(all_locations[location]['Broken'])/all_locations[location]['Total']
-                all_locations[location]['PercUnusable'] = 100.0*(all_locations[location]['Unusable'])/all_locations[location]['Total']
+                all_locations[location]['PercTotal'] = "%.0f" % (100.0 * (all_locations[location]['Usable'])/all_locations[location]['Total'])
+                all_locations[location]['PercBroken'] = "%.0f" % (100.0 * (all_locations[location]['Broken'])/all_locations[location]['Total'])
+                all_locations[location]['PercUnusable'] = "%.0f" % (100.0 * (all_locations[location]['Unusable'])/all_locations[location]['Total'])
 
                 #Keep track of css colors for each location threshold. (i.e. green = 95% in one area, etc..)
                 try:
