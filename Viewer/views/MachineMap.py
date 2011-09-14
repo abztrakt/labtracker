491003293444f7b1f62a007b7c5ef9a6682b9bfb
Quinn Walker-Phelan <qnnwp@uw.edu>
2011-09-13 17:23:01 -0700

    changed the machine map so that the available opacity is lower but doesn't remove the broken status's outline Fixes #249

--- Viewer/views/MachineMap.py
+++ Viewer/views/MachineMap.py
@@ -42,8 +42,7 @@ def show(request, view_name):
         PRE: Takes a datetime object and will gets machines added after that date
         POST: Returns machine information in JSON format
         """
-        items = view.getMappedItems().exclude(last_modified__lte=last,machine__item__last_modified__lte=last)
-        
+        items = view.getMappedItems().exclude(machine__item__last_modified__lte=last)
         ret_data = {}
 
         for item in items:
@@ -155,8 +154,6 @@ def show(request, view_name):
                 'status':status,
                 'broken':broken,
                 'verified': verified,
-                'vypos': item.ypos+2,
-                'vxpos': item.xpos+2,
                 'name': item.machine.item.name,
                 'wall_port': item.machine.item.wall_port,
                 'mac1': item.machine.item.mac1,
