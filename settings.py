6880ba4ef966630f0beb9cc242daff2c7063f532
Craig M. Stimmel <cstimmel@uw.edu>
2011-12-12 15:45:16 -0800

    Added a try/except that saves us if we're just doing dev work and don't have MySQLdb installed.

--- settings.py
+++ settings.py
@@ -24,16 +24,19 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'labtracker',                      # Or path to database file if using sqlite3.
-        'USER': 'labtracker',                      # Not used with sqlite3.
-        'PASSWORD': 'MYSECRETPASSWORD',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+try:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'labtracker',                      # Or path to database file if using sqlite3.
+            'USER': 'labtracker',                      # Not used with sqlite3.
+            'PASSWORD': 'MYSECRETPASSWORD',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
     }
-}
+except ImproperlyConfigured:
+    pass
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
